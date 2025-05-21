121e88acfbdbfe575226009b88f9fdb674044b0c
emmanuelologe <epicraez>
2025-05-22 00:16:30 +0100

    index update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => {res.send('hello Worlds');});
+router.get('/', (req, res) => {res.send('hello World');});
 
 router.use('/contacts', require('./contacts'));
 
